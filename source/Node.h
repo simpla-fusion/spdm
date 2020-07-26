a58c2041b0938198f7820cd2947d2863b7e30b6b
YUZhi <yuzhi@ipp.ac.cn>
2020-07-26 11:24:26 +0800

    snapshot

--- source/Node.h
+++ source/Node.h
@@ -13,195 +13,194 @@
 #include <vector>
 namespace sp
 {
-struct XPath;
 
-class Entry;
+class Node;
 
-template <typename TTree>
-class NodeObject
+typedef HierarchicalTree<
+    Node,
+    HierarchicalTreeObjectPolicy,                                //Object
+    HierarchicalTreeArrayPolicy,                                 //Array
+    std::tuple<std::shared_ptr<void>, int, std::vector<size_t>>, //Block
+    std::string,                                                 //String,
+    bool,                                                        //Boolean,
+    int,                                                         //Integer,
+    long,                                                        //Long,
+    float,                                                       //Float,
+    double,                                                      //Double,
+    std::complex<double>,                                        //Complex,
+    std::array<int, 3>,                                          //IntVec3,
+    std::array<long, 3>,                                         //LongVec3,
+    std::array<float, 3>,                                        //FloatVec3,
+    std::array<double, 3>,                                       //DoubleVec3,
+    std::array<std::complex<double>, 3>,                         //ComplexVec3,
+    std::any                                                     //Other
+    >
+    tree_type;
+
+class Node : public tree_type
 {
 public:
-    typedef TTree tree_type;
+    typedef tree_type base_type;
 
-    NodeObject(tree_type* parent);
-    ~NodeObject() = default;
-
-    size_t size() const;
-
-    void clear();
-
-    const tree_type& at(const std::string&) const;
-
-    template <typename... Args>
-    std::pair<iterator, bool> try_emplace(const std::string&, Args&&... args);
-
-    template <typename... Args>
-    iterator find(const std::string&, Args&&... args);
-
-    template <typename... Args>
-    const iterator find(const std::string&, Args&&... args) const;
-
-    template <typename... Args>
-    int erase(const std::string&, Args&&... args);
-
-private:
-    std::shared_ptr<Entry> m_entry_;
-};
-
-template <typename TTree>
-class NodeArray
-{
-public:
-    typedef TTree tree_type;
-
-    size_t size() const;
-
-    void resize(size_t);
-
-    void clear();
-
-    template <typename... Args>
-    tree_type& emplace_back(Args&&... args);
-
-    void pop_back();
-
-    tree_type& at(int);
-
-    const tree_type& at(int) const;
-};
-
-typedef HierarchicalTree<EntryObject, EntryArray> Node;
-class Node : public
-{
-private:
-    std::string m_path_;
-
-public:
-    typedef Node this_type;
-    typedef HierarchicalTree<EntryObject, EntryArray> base_type;
-
-    class cursor;
-
-    explicit Node(const std::string& uri = "");
-
-    explicit Node(const std::shared_ptr<Entry>& p, const std::string& prefix = "");
-
-    Node(const this_type&);
-
-    Node(this_type&&);
-
-    ~Node();
-
-    void swap(this_type&);
-
-    this_type& operator=(this_type const& other);
-
-    bool operator==(this_type const& other) const;
-
-    operator bool() const { return !is_null(); }
-
-    void resolve();
-
-    //
-
-    std::string path() const;
-
-    std::string name() const;
-
-    const Node& value() const;
-
-    Node& value();
-
-    // attributes
-
-    bool has_attribute(const std::string& name) const;
-
-    const Entry::element_t get_attribute_raw(const std::string& name) const;
-
-    void set_attribute_raw(const std::string& name, const Entry::element_t& value);
-
-    void remove_attribute(const std::string& name);
-
-    template <typename V>
-    const V get_attribute(const std::string& name)
+    enum DataType
     {
-        return std::get<V>(get_attribute_raw(name));
+        Null,
+        Object,
+        Array,
+        Block,
+        String,
+        Bool,
+        Int,
+        Long,
+        Float,
+        Double,
+        Complex,
+        IntVec3,
+        LongVec3,
+        FloatVec3,
+        DoubleVec3,
+        ComplexVec3,
+        Other
     };
 
-    template <typename V, typename U>
-    void set_attribute(const std::string& name, const U& value)
-    {
-        set_attribute_raw(name, Entry::element_t{V(value)});
-    }
+    template <typename... Args>
+    Node(Args&&... args)
+        : base_type(nullptr, std::integral_constant<int, base_type::OBJECT_TAG>(), std::forward<Args>(args)...) {}
 
-    std::map<std::string, Entry::element_t> attributes() const;
-
-    //----------------------------------------------------------------------------------
-    // level 0
-    //
-    // as leaf
-
-    // as Tree
-    // as container
-
-    Node* parent() const;
-
-    // as array
-
-    //-------------------------------------------------------------------
-    // level 1
-    // xpath
-
-    Node insert(const XPath&);
-
-    cursor find(const XPath&) const;
-
-    typedef std::function<bool(const Node&)> pred_fun;
-
-    cursor find(const pred_fun&) const;
-
-    int update(cursor&, const Node&);
-
-    int remove(cursor&);
-};
-
-class Node::cursor
-{
-public:
-    cursor();
-
-    cursor(Entry*);
-
-    cursor(const std::shared_ptr<Entry>&);
-
-    cursor(const cursor&);
-
-    cursor(cursor&&);
-
-    ~cursor() = default;
-
-    bool operator==(const cursor& other) const;
-
-    bool operator!=(const cursor& other) const;
-
-    operator bool() const { return !is_null(); }
-
-    bool is_null() const;
-
-    Node operator*();
-
-    std::unique_ptr<Node> operator->();
-
-    cursor& operator++();
-
-    cursor operator++(int);
-
-private:
-    std::shared_ptr<Entry> m_entry_;
+    ~Node() = default;
 };
 
 std::ostream& operator<<(std::ostream& os, Node const& Node);
 
 } // namespace sp
+
+// class Node
+// {
+// private:
+//     std::string m_path_;
+
+// public:
+//     typedef Node this_type;
+//     typedef HierarchicalTree<EntryObject, EntryArray> base_type;
+
+//     class cursor;
+
+//     explicit Node(const std::string& uri = "");
+
+//     explicit Node(const std::shared_ptr<Entry>& p, const std::string& prefix = "");
+
+//     Node(const this_type&);
+
+//     Node(this_type&&);
+
+//     ~Node();
+
+//     void swap(this_type&);
+
+//     this_type& operator=(this_type const& other);
+
+//     bool operator==(this_type const& other) const;
+
+//     operator bool() const { return !is_null(); }
+
+//     void resolve();
+
+//     //
+
+//     std::string path() const;
+
+//     std::string name() const;
+
+//     const Node& value() const;
+
+//     Node& value();
+
+//     // attributes
+
+//     bool has_attribute(const std::string& name) const;
+
+//     const Entry::element_t get_attribute_raw(const std::string& name) const;
+
+//     void set_attribute_raw(const std::string& name, const Entry::element_t& value);
+
+//     void remove_attribute(const std::string& name);
+
+//     template <typename V>
+//     const V get_attribute(const std::string& name)
+//     {
+//         return std::get<V>(get_attribute_raw(name));
+//     };
+
+//     template <typename V, typename U>
+//     void set_attribute(const std::string& name, const U& value)
+//     {
+//         set_attribute_raw(name, Entry::element_t{V(value)});
+//     }
+
+//     std::map<std::string, Entry::element_t> attributes() const;
+
+//     //----------------------------------------------------------------------------------
+//     // level 0
+//     //
+//     // as leaf
+
+//     // as Tree
+//     // as container
+
+//     Node* parent() const;
+
+//     // as array
+
+//     //-------------------------------------------------------------------
+//     // level 1
+//     // xpath
+
+//     Node insert(const XPath&);
+
+//     cursor find(const XPath&) const;
+
+//     typedef std::function<bool(const Node&)> pred_fun;
+
+//     cursor find(const pred_fun&) const;
+
+//     int update(cursor&, const Node&);
+
+//     int remove(cursor&);
+// };
+
+// class Node::cursor
+// {
+// public:
+//     cursor();
+
+//     cursor(Entry*);
+
+//     cursor(const std::shared_ptr<Entry>&);
+
+//     cursor(const cursor&);
+
+//     cursor(cursor&&);
+
+//     ~cursor() = default;
+
+//     bool operator==(const cursor& other) const;
+
+//     bool operator!=(const cursor& other) const;
+
+//     operator bool() const { return !is_null(); }
+
+//     bool is_null() const;
+
+//     Node operator*();
+
+//     std::unique_ptr<Node> operator->();
+
+//     cursor& operator++();
+
+//     cursor operator++(int);
+
+// private:
+//     std::shared_ptr<Entry> m_entry_;
+// };
 
 #endif // SP_NODE_H_
--- tests/test_hdata.cpp
+++ tests/test_hdata.cpp
@@ -6,7 +6,7 @@
 
 TEST_CASE("HData ", "[HierarchicalTree]")
 {
-    sp::HierarchicalTree<> attr;
+    sp::HierarchicalNode attr;
 
     attr["A"] = 1234.5;
 
@@ -15,39 +15,39 @@ TEST_CASE("HData ", "[HierarchicalTree]")
 
 TEST_CASE("Object", "[HierarchicalTree]")
 {
-    sp::HierarchicalTree<> node;
+    sp::HierarchicalNode node;
     using namespace std::literals;
 
     node["A"] = "1234"s;
 
-    node["B"].template set_value<sp::DataType::Float>(3.14);
+    node["B"].template set_value<sp::HierarchicalNode::DataType::Float>(3.14);
 
     REQUIRE(node.size() == 2);
 
-    REQUIRE(node["A"].template get_value<sp::DataType::String>() == "1234");
+    REQUIRE(node["A"].template get_value<sp::HierarchicalNode::DataType::String>() == "1234");
 
-    REQUIRE(node["B"].template get_value<sp::DataType::Float>() == 3.14f);
+    REQUIRE(node["B"].template get_value<sp::HierarchicalNode::DataType::Float>() == 3.14f);
 }
-TEST_CASE("Path", "[HierarchicalTree]")
-{
-    sp::HierarchicalTree<> node;
-
-    node["D/E/F"].template set_value<double>(1.2345);
-
-    REQUIRE(node["D"]["E"]["F"].template get_value<sp::DataType::Double>() == 1.2345);
-}
-// TEST_CASE("Array", "[HierarchicalTree]")
+// TEST_CASE("Path", "[HierarchicalTree]")
 // {
-//     sp::HierarchicalTree node;
+//     sp::HierarchicalNode node;
 
-//     node["C"].resize(2);
+//     node["D/E/F"].template set_value<double>(1.2345);
 
-//     node["C"][1] = (5);
-
-//     node["C"][0].emplace<double>(6.0);
-
-//     REQUIRE(node["C"].size() == 2);
-
-//     REQUIRE(node["C"][1].get_value<sp::DataType::Integer>() == 5);
-//     REQUIRE(node["C"][0].get_value<sp::DataType::Double>() == 6.0);
+//     REQUIRE(node["D"]["E"]["F"].template get_value<sp::HierarchicalNode::DataType::Double>() == 1.2345);
 // }
+TEST_CASE("Array", "[HierarchicalTree]")
+{
+    sp::HierarchicalNode node;
+
+    node["C"].resize(2);
+
+    node["C"][1] = (5);
+
+    node["C"][0].set_value<double>(6.0);
+
+    REQUIRE(node["C"].size() == 2);
+
+    REQUIRE(node["C"][1].get_value<sp::HierarchicalNode::DataType::Int>() == 5);
+    REQUIRE(node["C"][0].get_value<sp::HierarchicalNode::DataType::Double>() == 6.0);
+}
