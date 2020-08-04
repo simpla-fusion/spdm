bc47e2c91d30c6125c799ea92d7a95ab75275970
Salmon <yuzhi@ipp.ac.cn>
2020-08-04 14:42:35 +0800

    snapshot

---
+++ source/db/XPath.cpp
@@ -0,0 +1,127 @@
+#include "XPath.h"
+#include "../utility/Logger.h"
+#include <regex>
+#include <string>
+namespace sp::db
+{
+
+/**
+     * https://www.ietf.org/rfc/rfc3986.txt
+     * 
+     *    scheme    = $2
+     *    authority = $4
+     *    path      = $5
+     *    query     = $7
+     *    fragment  = $9
+     * 
+     * 
+    */
+static const std::regex url_pattern("(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\\?([^#]*))?(#(.*))?");
+
+static const std::regex xpath_pattern("([a-zA-Z_\\$][^/#\\[\\]]*)(\\[([^\\[\\]]*)\\])?");
+
+std::string urljoin(std::string const& prefix, std::string const& path)
+{
+    std::string res;
+    if (path[0] == '/')
+    {
+        res = path;
+    }
+    else
+    {
+        auto pos = prefix.rfind('/');
+        if (pos != std::string::npos)
+        {
+            res = prefix.substr(0, pos) + "/" + path;
+        }
+        else
+        {
+            res = path;
+        }
+    }
+    return res;
+}
+
+std::tuple<std::string, std::string, std::string, std::string, std::string> urlparse(std::string const& url)
+{
+    std::smatch m;
+    if (!std::regex_match(url, m, url_pattern))
+    {
+        throw std::runtime_error("illegal request! " + url);
+    }
+
+    std::string scheme = m[2].str();
+    std::string authority = m[4].str();
+    std::string path = m[5].str();
+    std::string query = m[7].str();
+    std::string fragment = m[9];
+    return std::make_tuple(scheme, authority, path, query, fragment);
+}
+
+XPath::XPath(const std::string& url)
+{
+    std::smatch m;
+    if (!std::regex_match(url, m, url_pattern))
+    {
+        throw std::runtime_error("illegal request! " + url);
+    }
+
+    m_scheme_ = m[2].str();
+    m_authority_ = m[4].str();
+    m_query_ = m[7].str();
+    m_fragment_ = m[9];
+
+    std::string path = m[5].str();
+
+    int pos = 0;
+
+    do
+    {
+        auto pend = path.find("/", pos);
+
+        if (pend == std::string::npos)
+        {
+            append(path.substr(pos));
+            pos = pend;
+        }
+        else
+        {
+            append(path.substr(pos, pend - pos));
+            pos = pend + 1;
+        }
+    } while (pos != std::string::npos);
+}
+
+XPath::XPath(const XPath& other)
+    : m_scheme_(other.m_scheme_),
+      m_authority_(other.m_authority_),
+      m_path_(other.m_path_),
+      m_query_(other.m_query_),
+      m_fragment_(other.m_fragment_)
+{
+}
+
+XPath::XPath(XPath&& other)
+    : m_scheme_(std::move(other.m_scheme_)),
+      m_authority_(std::move(other.m_authority_)),
+      m_path_(std::move(other.m_path_)),
+      m_query_(std::move(other.m_query_)),
+      m_fragment_(std::move(other.m_fragment_))
+{
+}
+
+XPath::~XPath() {}
+
+std::string XPath::str() const
+{
+    NOT_IMPLEMENTED;
+    return "";
+}
+void XPath::append(const std::string& path)
+{
+    std::cout << FILE_LINE_STAMP << path << std::endl;
+    m_path_.emplace_back(path);
+}
+void XPath::append(int idx) { m_path_.emplace_back(idx); }
+void XPath::append(int b, int e, int seq) { m_path_.emplace_back(std::make_tuple(b, e, seq)); }
+} // namespace sp::db
---
+++ source/db/XPath.h
@@ -0,0 +1,104 @@
+#ifndef SPDB_XPath_h_
+#define SPDB_XPath_h_
+
+#include <algorithm>
+#include <any>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <stdlib.h>
+#include <string>
+#include <variant>
+#include <vector>
+namespace sp::db
+{
+/**
+ *  - URL : @ref  https://www.ietf.org/rfc/rfc3986.txt
+ *  - JSON pointer  
+ *  - XXPath
+ * 
+*/
+class XPath
+{
+public:
+    typedef std::variant<std::string /* key or query*/, int /* index */, std ::tuple<int, int, int> /* slice */> element;
+
+    enum type_tags
+    {
+        Key,
+        Index,
+        Slice
+    };
+
+    typedef XPath this_type;
+    explicit XPath(const std::string&);
+    XPath(const XPath&);
+    XPath(XPath&&);
+    ~XPath();
+
+    template <typename FirstSegment, typename... Others>
+    XPath(const XPath& other, FirstSegment&& seg, Others&&... others)
+        : XPath(other) { append(std::forward<FirstSegment>(seg), std::forward<Others>(others)...); }
+
+    template <typename FirstSegment, typename... Others>
+    XPath(XPath&& other, FirstSegment&& seg, Others&&... others)
+        : XPath(std::forward<XPath>(other)) { append(std::forward<FirstSegment>(seg), std::forward<Others>(others)...); }
+
+    void swap(this_type& other)
+    {
+        std::swap(m_scheme_, other.m_scheme_);
+        std::swap(m_authority_, other.m_authority_);
+        std::swap(m_path_, other.m_path_);
+        std::swap(m_query_, other.m_query_);
+        std::swap(m_fragment_, other.m_fragment_);
+    }
+
+    this_type& operator=(const this_type& other)
+    {
+        this_type(other).swap(*this);
+        return *this;
+    }
+
+    void append(const std::string& path);
+    void append(int idx);
+    void append(int b, int e, int seq = 1);
+
+    std::string str() const;
+
+    const std::string& scheme() const { return m_scheme_; }
+    void scheme(const std::string& s) { m_scheme_ = s; }
+
+    const std::string& authority() const { return m_authority_; }
+    void authority(const std::string& s) { m_authority_ = s; }
+
+    const std::string& query() const { return m_query_; }
+    void query(const std::string& s) { m_query_ = s; }
+
+    const std::string& fragment() const { return m_fragment_; }
+    void fragment(const std::string& s) { m_fragment_ = s; }
+
+    template <typename Key>
+    this_type operator[](const Key& key) const { return XPath(*this, key); }
+
+    this_type operator/(const std::string& key) const { return XPath(*this, key); }
+
+    size_t size() const { return m_path_.size(); }
+
+    auto begin() const { return m_path_.begin(); }
+
+    auto end() const { return m_path_.end(); }
+
+private:
+    std::string m_scheme_;
+    std::string m_authority_;
+    std::vector<element> m_path_;
+    std::string m_query_;
+    std::string m_fragment_;
+};
+namespace literals
+{
+inline XPath operator"" _p(const char* s, std::size_t) { return XPath(s); }
+} // namespace literals
+} // namespace sp::db
+#endif //SPDB_XPath_h_
