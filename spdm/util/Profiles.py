15ab0c0b8053383606b4a9df6bf9f730d77d23ff
Salmon <yuzhi@ipp.ac.cn>
2020-12-18 09:38:52 +0800

    test Profile

--- spdm/util/Profiles.py
+++ spdm/util/Profiles.py
@@ -19,7 +19,6 @@ from spdm.util.logger import logger
 
 
 class Profile(np.ndarray):
-
     def __array_finalize__(self, obj):
         if obj is None:
             return
@@ -34,44 +33,29 @@ class Profile(np.ndarray):
 
     @property
     def is_constant(self):
-        return self.shape == ()
+        return self.shape == () and self.__class__ is not ProfileFunction
 
-    def evaluate(self):
-        pass
-
-    def at(self, idx):
-        return self.value[idx]
-
-    def __getitem__(self, idx):
-        res = self.value[idx]
-        if isinstance(res, Profile):
-            res._axis = self._axis[idx]
-        elif isinstance(res, np.ndarray):
-            res = Profile(res, axis=self._axis[idx])
-        return res
+    @property
+    def value(self):
+        if self.is_constant:
+            return self.item()
+        else:
+            return self.view(np.ndarray)
 
     def __setitem__(self, *args):
-        self.evaluate()
         super().__setitem__(*args)
-        if hasattr(self, "interpolate"):
-            del self.interpolate
+        if hasattr(self, "as_function"):
+            del self.as_function
         if hasattr(self, "derivative"):
             del self.derivative
         if hasattr(self, "dln"):
             del self.dln
 
-    def iter_over(self, axis):
-        if self.is_constant:
-            for x in axis.flat:
-                yield self.item()
-        elif axis is self._axis:
-            yield from self.flat
-        elif isinstance(axis, np.ndarray):
-            for x in axis.flat:
-                yield self.eval(x)
-        else:
-            for x in axis:
-                yield self.eval(x)
+    def __getitem__(self, idx):
+        res = super().__getitem__(idx)
+        if isinstance(res, np.ndarray):
+            res = res.view(np.ndarray)
+        return res
 
     @property
     def description(self):
@@ -82,15 +66,7 @@ class Profile(np.ndarray):
         return self._axis
 
     @cached_property
-    def value(self):
-        if self.is_constant:
-            return self.item()
-        else:
-            self.evaluate()
-            return self.view(np.ndarray)
-
-    @cached_property
-    def interpolate(self):
+    def as_function(self):
         axis = self._axis.view(np.ndarray)
         data = self.value
         try:
@@ -101,59 +77,42 @@ class Profile(np.ndarray):
         return res
 
     def copy(self, other):
-        if isinstance(other, ProfileExpression):
+        if isinstance(other, Profile):
             if self._axis is other._axis:
                 np.copyto(self, other.value)
             else:
-                np.copyto(self, other.resample(self._axis))
-        elif isinstance(other, Profile):
-            np.copyto(self, other.resample(self._axis))
-        elif isinstance(other, np.ndarray) and self.shape == other.shape:
-            np.copyto(self, other)
-        elif isinstance(other, (int, float, type(None))):
+                np.copyto(self, other(self._axis))
+        elif not isinstance(other, np.ndarray):
             self.fill(other)
-        elif not isinstance(other, np.ndarray) and not other:
-            self.fill(np.nan)
+        elif self.shape == other.shape:
+            np.copyto(self, other)
         else:
             raise ValueError(f"Can not copy object! {type(other)} {other} ")
 
-    def resample(self, axis, *args, **kwargs):
-        assert(isinstance(axis, np.ndarray))
-        return self.eval(axis, *args, **kwargs)
-
-    def __call__(self, *args, **kwargs):
-        if self.is_constant:
-            return self.value
-        else:
-            return self.eval(*args, **kwargs)
-
-    def eval(self,  axis, *args, **kwargs):
-        if axis is self._axis:
+    def __call__(self, x_axis=None, *args, **kwargs):
+        if x_axis is self._axis or x_axis is None:
             return self
 
-        ufunc = getattr(self, "_ufunc", None) or self.interpolate
-
-        if ufunc is None:
-            raise RuntimeError(f"Function is not defined")
-
-        res = ufunc(axis, *args, **kwargs)
+        res = self.as_function(x_axis)
 
         if isinstance(res, Profile):
             if not hasattr(res, "_axis"):
-                res._axis = axis
+                res._axis = x_axis
         elif isinstance(res, np.ndarray):
             if len(res.shape) == 0:
                 res = res.item()
             elif res.size == 1:
                 res = res[0]
             else:
-                res = Profile(res, axis=axis, description=self.description)
+                res = res.view(Profile)
+                res._axis = x_axis
+                res._description = self.description
 
         return res
 
         # def derivative_n(self, n, *args, **kwargs):
         #     self.evaluate()
-        #     return Profile(self.interpolate.derivative(n=n)(self._axis), axis=self._axis)
+        #     return Profile(self.as_function.derivative(n=n)(self._axis), axis=self._axis)
         # func = getattr(self, "_ufunc", None)
         # if func is None:
         # elif callable(func) or isinstance(func, types.BuiltinFunctionType):
@@ -175,12 +134,12 @@ class Profile(np.ndarray):
         return Profile(self.derivative.value/self.value, axis=self._axis)
 
     @cached_property
-    def cum_integral(self):
+    def integral(self):
         value = scipy.integrate.cumtrapz(self.axis, self.value, initial=0.0)
         return Profile(value, axis=self.axis)
 
     @cached_property
-    def inv_cum_integral(self):
+    def inv_integral(self):
         value = scipy.integrate.cumtrapz(self.axis[::-1], self.value[::-1], initial=0.0)[::-1]
         return Profile(value, axis=self.axis)
 
@@ -200,9 +159,9 @@ class Profile(np.ndarray):
         #     start = self._axis
         # if stop is None:
         #     stop = self._axis
-        # func = getattr(self, "_ufunc", None) or self.interpolate
+        # func = getattr(self, "_ufunc", None) or self.as_function
         # if func is None:
-        #     spl = self.interpolate
+        #     spl = self.as_function
         #     if isinstance(start, np.ndarray) and isinstance(stop, np.ndarray):
         #         raise ValueError(f"Illegal arguments! start is {type(start)} ,end is {type(end)}")
         #     elif isinstance(start, np.ndarray):
@@ -214,216 +173,130 @@ class Profile(np.ndarray):
         # else:
 
 
-class ProfileExpression(Profile):
-    def __init__(self, ufunc, method=None, *args, func_args=None, func_kwargs=None, **kwargs):
+class ProfileFunction(Profile):
+
+    def __init__(self, ufunc,   *args,  **kwargs):
         super().__init__(*args, **kwargs)
+
+        if not isinstance(ufunc, np.ufunc):
+            ufunc = np.vectorize(ufunc)
+
+        self._ufunc = ufunc
+
+    def __call__(self, x=None):
+        if x is None:
+            x = self._axis
+        if x is None:
+            raise ValueError(f" x is None !")
+
+        res = self._ufunc(x)
+        if isinstance(res, np.ndarray) and not isinstance(res, Profile):
+            res = res.view(Profile)
+            res._axis = x
+        return res
+
+    def __getitem__(self, idx):
+        return self._ufunc(self._axis[idx])
+
+    @property
+    def value(self):
+        if self.shape == ():
+            try:
+                self.resize(self._axis.size, refcheck=True)
+                self.reshape(self._axis.shape)
+            except Exception:
+                res = self._ufunc(self._axis)
+            else:
+                np.copyto(self, self._ufunc(self._axis))
+                res = self.view(np.ndarray)
+        else:
+            res = self.view(np.ndarray)
+        return res
+
+    @cached_property
+    def derivative(self):
+        return Profile(np.gradient(self.value)/np.gradient(self._axis), axis=self._axis)
+
+    @cached_property
+    def dln(self, *args, **kwargs):
+        r"""
+            .. math:: d\ln f=\frac{df}{f}
+        """
+        return Profile(self.derivative.value/self.value, axis=self._axis)
+
+    @cached_property
+    def integral(self):
+        value = scipy.integrate.cumtrapz(self.axis, self.value, initial=0.0)
+        return Profile(value, axis=self.axis)
+
+    @cached_property
+    def inv_integral(self):
+        value = scipy.integrate.cumtrapz(self.axis[::-1], self.value[::-1], initial=0.0)[::-1]
+        return Profile(value, axis=self.axis)
+
+
+class ProfileExpression(Profile):
+
+    @staticmethod
+    def enable(flag=True):
+        if flag:
+            Profile.__array_ufunc__ = lambda s, ufunc, method, *args, **kwargs: ProfileExpression(
+                ufunc, method, func_args=args, func_kwargs=kwargs)
+        elif hasattr(Profile, "__array_ufunc__"):
+            del Profile.__array_ufunc__
+
+    def __init__(self, ufunc, method, *args, func_args=None, func_kwargs=None, axis=None, **kwargs):
         self._ufunc = ufunc
         self._method = method
-        self._op = getattr(ufunc, method) if method is not None else ufunc
         self._args = []
 
         for arg in (func_args or []):
-            if isinstance(arg, Profile):
-                self._args.append(arg)
-            elif isinstance(arg, np.ndarray):
-                assert(arg.shape == self.axis.shape)
-                arg = arg.view(Profile)
-                arg._axis = self.axis
-                self._args.append(arg)
-            else:
-                self._args.append(arg)
+            if isinstance(arg, Profile) and axis is None:
+                axis = arg.axis
+            self._args.append(arg)
 
         self._kwargs = func_kwargs or {}
 
-    def at(self, idx):
-        return self.eval(self._axis[idx])
+        super().__init__(*args, **kwargs)
+
+        self._axis = axis
 
     def __str__(self):
         return self.__repr__()
 
     def __repr__(self):
-        # self.evaluate()
-        # return super().__repr__()
         return f"<{self.__class__.__name__} ufunc={self._ufunc} method={self._method}>"
 
     def __call__(self, x_axis=None):
-        if not isinstance(x_axis, np.ndarray):
-            res = self.eval(x_axis)
-        elif self.is_constant:
-            return self.value
-        else:
-            res = np.ndarray(x_axis.shape)
+        if x_axis is None:
+            x_axis = self._axis
+        args = []
+        for arg in self._args:
+            if isinstance(arg,  Profile):
+                args.append(arg(x_axis))
+            else:
+                args.append(arg)
 
-            for idx, v in enumerate(self.iter_over(x_axis)):
-                if isinstance(v, Profile):
-                    logger.debug(v)
-                    v = v.value
-                try:
-                    res[idx] = v
-                except Exception as error:
-                    logger.debug(v)
-                    raise error
-            # with np.nditer(res, op_flags=['readwrite']) as it:
-            #     for x in it:
-            #         v = next(value_it)
-            #         try:
-            #             x[...] = v
-            #         except Exception as error:
-            #             raise error
+        res = getattr(self._ufunc, self._method)(*args, **self._kwargs)
 
+        if isinstance(res, np.ndarray):
             res = res.view(Profile)
             res._axis = x_axis
-            res._description = self._description
-
-        if isinstance(res, Profile):
-            pass
-        elif isinstance(res, np.ndarray):
-            res = Profile(res, axis=x_axis)
 
         return res
 
     def __getitem__(self, idx):
-
-        return self.eval(self._axis[idx])
-
-    def eval(self, x):
-        if isinstance(x, (np.ndarray, collections.abc.Sequence)):
-            pass
-        else:
-            x = [x]
-
-        # args = [(arg(x) if callable(arg) else arg) for arg in self._args]
         args = []
         for arg in self._args:
-            if callable(arg):
-                args.append(arg(x))
+            if isinstance(arg, (Profile, np.ndarray)):
+                args.append(arg[idx].view(np.ndarray))
             else:
                 args.append(arg)
-            if not isinstance(arg, np.ndarray) and np.isnan(arg):
-                logger.error(arg)
-                raise ValueError(self)
 
-        [v for v in self.iter_over(x)]
+        return getattr(self._ufunc, self._method)(*args, **self._kwargs)
 
-        return self._op(*args, **self._kwargs)
-
-    def iter_over(self, axis):
-        args_it = []
-        for arg in self._args:
-            if isinstance(arg, Profile):
-                args_it.append((arg.iter_over(axis), True))
-            else:
-                args_it.append((arg, False))
-
-        for _ in np.nditer(axis):
-            s_args = []
-            for arg, iterable in args_it:
-                if iterable:
-                    s_args.append(next(arg))
-                else:
-                    s_args.append(arg)
-
-            if self._method is not None:
-                res = getattr(self._ufunc, self._method)(*s_args, **self._kwargs)
-            else:
-                res = self._ufunc(self._axis)
-
-            if isinstance(res, ProfileExpression):
-                res = res.value
-                logger.debug(s_args)
-                logger.debug([s for s in map(type, s_args)])
-                logger.debug((self._ufunc, self._method))
-
-            yield res
-            # yield self._op(* [(next(arg) if iterable else arg) for arg, iterable in args_it], **self._kwargs)
-
-    def __iter__(self):
-        yield from self.iter_over(self._axis)
-
-    # @property
-    # def shape(self):
-    #     return self._axis.shape
-
-    @property
-    def flat(self):
-        yield from self.iter_over(self._axis)
-
-    def evaluate(self):
-        args = []
-        in_no = []
-        axis = self._axis
-        for i, input_ in enumerate(self._args):
-            if isinstance(input_, Profile):
-                in_no.append(i)
-                if axis is None:
-                    axis = getattr(input_, "axis", None)
-
-                if input_.axis is axis:
-                    args.append(input_.view(np.ndarray))
-                else:
-                    data = input_(axis)
-                    if not isinstance(data, np.ndarray):
-                        args.append(data)
-                    elif data.shape != ():
-                        args.append(data.view(np.ndarray))
-                    else:
-                        args.append(data.item())
-
-            else:
-                args.append(input_)
-
-        if super().shape != axis.shape:
-            self.resize(axis.size, refcheck=False)  #
-            self.reshape(axis.shape)
-            self._axis = axis
-
-        data = self.view(np.ndarray)
-        for idx, v in enumerate(data.flat):
-            data[idx] = v
-
-        # outputs = out
-        # out_no = []
-        # if outputs:
-        #     out_args = []
-        #     for j, output in enumerate(outputs):
-        #         if isinstance(output, Profile):
-        #             out_no.append(j)
-        #             out_args.append(output.view(np.ndarray))
-        #         else:
-        #             out_args.append(output)
-        #     kwargs['out'] = tuple(out_args)
-        # else:
-        #     outputs = (None,) * ufunc.nout
-        # info = {}
-        # if in_no:
-        #     info['inputs'] = in_no
-        # if out_no:
-        #     info['outputs'] = out_no
-        # results = super(Profile, self).__array_ufunc__(ufunc, method, *args, **kwargs)
-        # results = getattr(ufunc, method)(*args, **kwargs)
-        # if results is NotImplemented:
-        #     return NotImplemented
-        # elif isinstance(results, np.ndarray) and results.shape != ():
-        #     results = np.asarray(results).view(Profile)
-        #     results._axis = axis
-        # return results
-        # if method == 'at':
-        #     if isinstance(inputs[0], Profile):
-        #         inputs[0].info = info
-        #     return
-        # if ufunc.nout == 1:
-        #     results = (results,)
-        # res = []
-        # for result, output in zip(results, outputs):
-        #     if output is not None:
-        #         res.append(output)
-        #     elif isinstance(result, np.ndarray) and result.shape != ():
-        #         p = np.asarray(result).view(Profile)
-        #         p._axis = axis
-        #         res.append(p)
-        #     else:
-        #         res.append(result)
-        # return res[0] if len(res) == 1 else res
+    def __setitem__(self, *args):
+        raise NotImplementedError
 
 
 def _profile_new_(cls, *args, axis=None, description=None,  **kwargs):
@@ -445,8 +318,12 @@ def _profile_new_(cls, *args, axis=None, description=None,  **kwargs):
 
     if len(args) == 0 or args[0] is None or isinstance(args[0],  (np.ndarray, int, float)):
         pass
-    else:
-        cls = ProfileExpression
+    elif not args[0]:
+        pass
+    elif len(args) == 1 and (callable(args[0]) or isinstance(args[0], np.ufunc)):
+        cls = ProfileFunction
+    # else:
+    #     raise TypeError(f"illegal value {type(args[0])} {args[0]}")
 
     obj = super(Profile, cls).__new__(cls, shape)
     obj._axis = axis
@@ -455,10 +332,7 @@ def _profile_new_(cls, *args, axis=None, description=None,  **kwargs):
 
 
 Profile.__new__ = _profile_new_
-Profile.__array_ufunc__ = lambda s, ufunc, method, *args, axis=None, **kwargs: ProfileExpression(
-    ufunc, method,
-    func_args=args, func_kwargs=kwargs,
-    axis=axis if axis is not None else s._axis)
+ProfileExpression.enable()
 
 
 class Profiles(AttributeTree):
@@ -496,7 +370,7 @@ class Profiles(AttributeTree):
     def __setitem__(self, key, value):
         v = self.__getitem__(key)
         if v is None:
-            self.__as_object__()[key] = self._create(value, name=key)
+            self.__as_object__()[key] = Profile(value, axis=self._axis,  description={"name": key})
         elif isinstance(v, Profile):
             if isinstance(value, (np.ndarray, int, float)):
                 v.copy(value)
--- tests/python/test_util_profile.py
+++ tests/python/test_util_profile.py
@@ -29,8 +29,67 @@ class TestProfile(unittest.TestCase):
 
         self.assertTrue(all(p[:] == y[:]))
 
+    def test_ufunc_expression(self):
+
+        def func(x): return x**2
+
+        x = np.linspace(0, 1.0, npoints)
+        y = np.vectorize(func)(x)*3.1456
+        p = Profile(func, axis=x)*3.1456
+
+        self.assertEqual(type(p), ProfileExpression)
+        self.assertTrue(all(p[:] == y[:]))
+
+    def test_expression(self):
+
+        x = np.linspace(0, 1.0, npoints)
+        y, z = np.random.rand(2, npoints)
+
+        r = y**2 + z/2.0
+
+        Y = Profile(y, axis=x)
+        Z = Profile(z, axis=x)
+
+        R = Y**2 + Z/2.0
+        self.assertEqual(type(R), ProfileExpression)
+       
+        self.assertTrue(all(R[:] == r[:]))
+
+    def test_profiles_setitem(self):
+
+        def func(x): return x**2
+
+        x = np.linspace(0, 1.0, npoints)
+
+        ps = Profiles(axis=x)
+        ps["a"] = func
+        y = func(x)
+
+        self.assertTrue(all(ps.a[:] == y[:]))
+
+    def test_profiles_setitem_expression(self):
+
+        def func(x): return x**2
+
+        x = np.linspace(0, 10, npoints)
+
+        ps = Profiles(axis=x)
+        y = func(x)
+        r = y+1.2345
+
+        Y = Profile(func, axis=x)
+
+        ps["a"] = Y+1.2345
+
+        self.assertTrue(type(Y + 1.2345), ProfileExpression)
+        logger.debug(Y)
+        logger.debug(ps.a[:])
+        logger.debug(r[:])
+        self.assertTrue(all(ps.a[:] == r[:]))
+
 
 if __name__ == '__main__':
-    from spdm.util.Profiles import Profile
+    from spdm.util.Profiles import Profile, ProfileFunction, ProfileExpression, Profiles
+    from spdm.util.logger import logger
 
     unittest.main()
