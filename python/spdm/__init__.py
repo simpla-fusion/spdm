0abf016d0e94b0e89ec1b3144e51d0fed37dca8f
salmon <yuzhi@ipp.ac.cn>
2021-09-09 10:13:53 +0800

    refactor(__init__.py): clean log

--- python/spdm/__init__.py
+++ python/spdm/__init__.py
@@ -1,16 +1,16 @@
-import numpy as np
-import scipy
-
 from .util.logger import logger
 
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
+import numpy as np
+import scipy
+logger.info(f"Using SciPy \t: {scipy.__version__}")
+logger.info(f"Using NumPy \t: {np.version.full_version}")
+
 __version__ = '0.0.0'
 
 
 logger.info(f"Using SpDB \t: {__version__}")
-logger.info(f"Using SciPy \t: {scipy.__version__}")
-logger.info(f"Using NumPy \t: {np.version.full_version}")
-
 
 # ENABLE_JAX = os.environ.get("SP_JAX", False)
 
