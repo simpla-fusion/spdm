a3dc63c6c04fc19e3bbfdbee8abaff798f02508c
salmon <yuzhi@ipp.ac.cn>
2022-11-25 18:24:46 +0800

    [update]demo mapping singal

--- examples/demo_mapping.py
+++ examples/demo_mapping.py
@@ -11,6 +11,7 @@ from spdm.data.Mapping import Mapping
 from fytok.modules.transport.Equilibrium import Equilibrium
 from fytok.modules.device.PFActive import PFActive
 from fytok.modules.device.Wall import Wall
+from fytok.modules.device.Magnetics import Magnetics
 from spdm import open_entry
 
 if __name__ == '__main__':
@@ -21,10 +22,10 @@ if __name__ == '__main__':
     entry = open_entry("file+mdsplus[EAST]:///home/salmon/workspace/data/~t/?tree_name=efit_east#38300",
                        mapping_path="/home/salmon/workspace/fytok_data/mapping")
 
-    for coil in entry.get(["pf_active", "coil"]):
-        print(coil.get("name"))
-        d = coil.get(["current","data"])
-        print(type(d))
+    magnetics = Magnetics(entry.get(["magnetics"]))
+    pf_active = PFActive(entry.get(["pf_active"]))
+
+    print(magnetics.b_field_tor_probe[2].field(1.2))
 
     fig = plt.figure()
     axis = fig.gca()
