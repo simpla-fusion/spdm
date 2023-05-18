19eb39ae9021a5e5139231541a4135f5b17daf4a
Salmon <yuzhi@ipp.ac.cn>
2023-05-18 09:23:40 +0800

    [fix]Profile.__init__

--- python/spdm/data/Profile.py
+++ python/spdm/data/Profile.py
@@ -17,8 +17,6 @@ class Profile(Node, Function[_T]):
 
     def __init__(self,  value: NumericType | Expression, *dims, **kwargs) -> None:
 
-        domain, kwargs = group_dict_by_prefix(kwargs, "coordinate")
-
         if isinstance(value, Expression):
             expr = value
             value = None
@@ -27,23 +25,25 @@ class Profile(Node, Function[_T]):
 
         Node.__init__(self, value, **kwargs)
 
-        if len(domain) > 0:
+        coordinats = {int(k[10:]): v for k, v in self._metadata.items() if k.startswith("coordinate")}
+
+        if len(coordinats) > 0:
             if not isinstance(self._parent, Container):
                 raise RuntimeError(f"Parent is None, can not determint the coordinates!")
 
-            domain_keys = [*domain.keys()]
-            domain_keys.sort()
-            domain_keys = [domain[c] for c in domain_keys]
+            coord_path = [*coordinats.keys()]
+            coord_path.sort()
+            coordinats = [coordinats[c] for c in coord_path]
 
             # FIXME: "1...N" is for IMAS dd
-            domain = tuple([(slice(None) if (c == "1...N") else self._find_node_by_path(c)) for c in domain_keys])
+            domain = tuple([(slice(None) if (c == "1...N") else self._find_node_by_path(c)) for c in coordinats])
         else:
             domain = dims
 
         Function.__init__(self, expr, *domain)
 
     def __str__(self) -> str: return Function.__str__(self)
-    
+
     @property
     def name(self) -> str: return self._metadata.get("name", 'unnamed')
 
