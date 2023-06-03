1f6c057494d2f664ca98cb48d83817d2305e3f3f
Salmon <yuzhi@ipp.ac.cn>
2023-06-03 17:47:45 +0800

    [working]

--- python/spdm/data/TimeSeries.py
+++ python/spdm/data/TimeSeries.py
@@ -9,13 +9,13 @@ from spdm.utils.logger import logger
 
 
 class TimeSlice(SpDict):
-    time: float = sp_property(unit='s', type='dynamic',default_value=0.0)
+    time: float = sp_property(unit='s', type='dynamic', default_value=0.0)
 
 
 _T = typing.TypeVar("_T")
 
 
-class TimeSeriesAoS(List[_T]):
+class TimeSeriesAoS(List[TimeSlice]):
     """
         A series of time slices, each time slice is a state of the system at a given time.
         Each slice is a dict .
@@ -24,9 +24,36 @@ class TimeSeriesAoS(List[_T]):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def update(self,  *args, dt=None, time=None, **kwargs):
+    @property
+    def current(self) -> TimeSlice: return self[-1]
+
+    def update(self, d=None, *args, **kwargs) -> TimeSlice:
         """
             update the last time slice, base on profiles_2d[-1].psi
         """
+        if len(self) == 0:
+            raise RuntimeError(f"TimeSeries is empty!")
 
-        pass
+        if hasattr(self[-1], "update"):
+            self[-1].update(d, *args, **kwargs)
+        elif isinstance(d, TimeSlice):
+            d._parent = self._parent
+            self[-1] = d
+        elif d is not None:
+            type_hint = self.__type_hint__()
+            self[-1] = type_hint(d, *args, **kwargs, parent=self._parent)
+
+        return self[-1]
+
+    def advance(self, d, *args,  **kwargs) -> TimeSlice:
+
+        if len(self) > 0 and hasattr(self[-1], "advance"):
+            new_obj = self[-1].advance(d, *args, **kwargs)
+        elif isinstance(d, TimeSlice):
+            new_obj = d
+            new_obj._parent = self._parent
+        else:
+            type_hint = self.__type_hint__()
+            new_obj = type_hint(d, *args, **kwargs, parent=self._parent)
+        self.append(new_obj)
+        return d
