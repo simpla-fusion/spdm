8ca8eeff47086f0fbf8d8180d7c2ff27b67e358d
Salmon <yuzhi@ipp.ac.cn>
2023-10-13 13:04:40 +0800

    [working]

--- python/spdm/__init__.py
+++ python/spdm/__init__.py
@@ -1,6 +1,8 @@
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
-__import__('pkg_resources').declare_namespace(__name__)
 
- 
-from .__version__ import __version__
- 
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("spdb")
+except PackageNotFoundError:
+    __version__ = "unknown version"
