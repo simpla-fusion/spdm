7931ced01654cf5ad52a3f214c1ba47facfcda70
Salmon <yuzhi@ipp.ac.cn>
2023-10-13 13:38:00 +0800

    [add]VERSION

--- python/spdm/__init__.py
+++ python/spdm/__init__.py
@@ -3,6 +3,6 @@ __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 from importlib.metadata import version, PackageNotFoundError
 
 try:
-    __version__ = version("spdb")
+    __version__ = version("spdm")
 except PackageNotFoundError:
     __version__ = "unknown version"
