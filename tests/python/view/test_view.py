16cc95bff8bc8c8c89de9eccbdc98c7e830a693d
Salmon <yuzhi@ipp.ac.cn>
2023-10-13 11:22:07 +0800

    [change]move tests togather

--- tests/python/view/test_view.py
+++ tests/python/view/test_view.py
@@ -2,7 +2,7 @@ import unittest
 from copy import deepcopy
 
 from spdm.utils.tags import _not_found_
-from spdm.views.View import display
+from spdm.view.View import display
 from spdm.utils.logger import logger
 from spdm.geometry.Circle import Circle
 
